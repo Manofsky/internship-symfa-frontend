9abf0629f0ad6a0959cd57b9208da49d056cd53a
Manofsky <clegrof@gmail.com>
2023-04-06 15:02:06 +0300

    feat: add footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './style.scss';
 import cx from 'classnames';
+import ic_shopping_bag from '../../assets/images/icons/ic_shopping_bag.svg';
 
 type Props = {
   className?: string,
@@ -9,9 +10,19 @@ type Props = {
 export default function Footer({ className }: Props) {
   return (
     <footer className={cx('footer', className)}>
-      <div className='footer__button'></div>
-      <div className='footer__navLeft'></div>
-      <div className='footer__navRight'></div>
+      <div className='footer__buttonWrapper'>
+        <button className='footer__buttonToCart'>
+          <img src={ic_shopping_bag} alt="icon" />
+        </button>
+      </div>
+      <div className='footer__navLeft'>
+        <button className='footer__navBtn footer__navBtn_homeIcon footer__navBtn_active'></button>
+        <button className='footer__navBtn footer__navBtn_bookIcon'></button>
+      </div>
+      <div className='footer__navRight'>
+        <button className='footer__navBtn  footer__navBtn_bellIcon'></button>
+        <button className='footer__navBtn  footer__navBtn_profileIcon'></button>
+      </div>
     </footer>
   )
 }
