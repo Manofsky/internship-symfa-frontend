a0fd39c5e65186c6061f9183355ade74fb378ea2
Manofsky <clegrof@gmail.com>
2023-04-22 22:50:54 +0300

    feat: add auth page & login component styles

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate, useLocation, Link } from 'react-router-dom';
 import './style.scss';
 import { useAppSelector } from '../../App/hooks';
 import { selectTheme } from '../../App/reducers/switchTheme';
 import { useLoginMutation } from '../../App/api/authApi';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 export default function Login() {
   const navigate = useNavigate();
@@ -15,6 +17,7 @@ export default function Login() {
     password: ''
   };
   const [loginData, setLoginData] = useState(defaultValue);
+  const [visibility, setVisibility] = useState(false);
 
   const [login, { isSuccess }] = useLoginMutation();
 
@@ -29,34 +32,50 @@ export default function Login() {
     login(loginData);
   };
 
+  function togglePassInput(e: any) {
+    e.preventDefault();
+    setVisibility(!visibility);
+  }
+
   return (
-    <div className={`login login_${themeValue}`}>
-      <div className={'login__pageWrapper'}>
-        <header className='login__header'>Login</header>
-        <main>
-          <form className='login__form' onSubmit={handleSubmit} >
-            <label>
-              Email Address<br />
-              <input
-                type="text"
-                name="login"
-                value={loginData.email}
-                onChange={(e) => setLoginData({ ...loginData, email: e.target.value })} />
-            </label>
-
-            <label>
-              Password<br />
-              <input
-                type="password"
-                name="password"
-                value={loginData.password}
-                onChange={(e) => setLoginData({ ...loginData, password: e.target.value })} />
-            </label>
-
-            <button type='submit'>Login</button>
-          </form>
-        </main>
+    <form className={`loginForm loginForm_${themeValue}`} onSubmit={handleSubmit} >
+      <div className='loginForm__inputBox'>
+        <label className='loginForm__label'>
+          Email Address<br />
+          <input
+            className='loginForm__input'
+            placeholder='type your email'
+            type="text"
+            name="login"
+            required
+            value={loginData.email}
+            onChange={(e) => setLoginData({ ...loginData, email: e.target.value })} />
+        </label>
       </div>
-    </div>
+
+      <div className='loginForm__inputBox'>
+        <label className='loginForm__label'>
+          Password<br />
+          <input
+            className='loginForm__input'
+            placeholder='type your password'
+            type={visibility ? "text" : "password"}
+            name="password"
+            required
+            value={loginData.password}
+            onChange={(e) => setLoginData({ ...loginData, password: e.target.value })} />
+          <button onClick={togglePassInput} className='loginForm__toggle'>
+            {visibility ? <VisibilityIcon /> : <VisibilityOffIcon />}
+          </button>
+        </label>
+      </div>
+
+      <Link to='#!' className='loginForm__link'>Forgot Password?</Link>
+
+      <div className='loginForm__buttonBox'>
+        <button type='submit' className='loginForm__button'>Login</button>
+      </div>
+
+    </form>
   )
 }
--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -1,22 +1,41 @@
 import React from 'react'
 import './style.scss'
-import { Link, Outlet } from 'react-router-dom'
-
-// type Props = {}
+import cx from 'classnames'
+import { useAppSelector } from '../../App/hooks'
+import { selectTheme } from '../../App/reducers/switchTheme'
+import { Link, Outlet, useLocation, useNavigate } from 'react-router-dom'
+import shoppinBag from '../../assets/images/icons/shopping-bag.svg'
 
 export default function Auth() {
-  return (
-    <div className='auth'>
-      <header className='auth__header'>
-        <h1>Corner Food</h1>
-        <p>Delivery App </p>
-        <nav>
-          <Link to='login'>Login</Link>
-          <Link to='signup'>Signup</Link>
-        </nav>
+  const themeValue = useAppSelector(selectTheme);
+  const location = useLocation();
+  const navigate = useNavigate()
+  const activeLogin = location.pathname === '/auth/login' ? 'authHeader__linkWrap_active' : '';
+  const activeSignup = location.pathname === '/auth/signup' ? 'authHeader__linkWrap_active' : '';
 
-      </header>
-      <Outlet />
+  const goToHome = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    navigate('/');
+  };
+
+  return (
+    <div className={`auth auth_${themeValue}`}>
+      <div className='auth__pageWrapper'>
+        <header className='auth__header authHeader'>
+          <button onClick={goToHome} className='authHeader__icon'>
+            <img src={shoppinBag} alt="icon" />
+          </button>
+          <div className='authHeader__heading'>
+            <h1 className='authHeader__title'>Corner Food</h1>
+            <p className='authHeader__subtitle'>Delivery App </p>
+          </div>
+          <nav className='authHeader__nav'>
+            <div className={cx('authHeader__linkWrap', activeLogin)}><Link to='login' className='authHeader__link'>Login</Link></div>
+            <div className={cx('authHeader__linkWrap', activeSignup)}><Link to='signup' className='authHeader__link'>Signup</Link></div>
+          </nav>
+        </header>
+        <Outlet />
+      </div>
     </div>
   )
 }
